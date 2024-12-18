0c0c1471edbe29fe54abf1f2a702a66517c7fc23
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2024-12-18 22:25:08 +0330

    Update dateage.py

--- dateage.py
+++ dateage.py
@@ -1,24 +1,15 @@
 import datetime
 birthdate=input()
 birthdate=birthdate.split('/')
-
 nowdate=datetime.datetime.now()
-
-
 nowdate=nowdate.strftime('%Y/%m/%d')
 nowdate=nowdate.split('/')
-
-
 daybirth=int(birthdate[2])
 monthbirth=int(birthdate[1])
 yearbirth=int(birthdate[0])
-
 daynow=int(nowdate[2])
 monthnow=int(nowdate[1])
 yearnow=int(nowdate[0])
-
-
-
 
 if monthbirth>12:
     print('WRONG')
